229fe04b83a73fd4044844b73fc04733a5689504
koonzwg <koonzwg@gmail.com>
2024-03-18 11:56:17 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by William Koonz on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by William Koonz on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+
+     var body: some View {
+         Button("Tap Me") {
+             // animationAmount += 1
+         }
+         .padding(50)
+         .background(.red)
+         .foregroundStyle(.white)
+         .clipShape(.circle)
+         .overlay(
+         Circle()
+            .stroke(.red)
+            .scaleEffect(animationAmount)
+            .opacity(2 - animationAmount)
+            .animation(
+                .easeInOut(duration: 1)
+                .repeatForever(autoreverses: false),
+                value: animationAmount
+            )
+         )
+         .onAppear {
+             animationAmount = 2
+         }
+     }
+ }
+
+#Preview {
+    ContentView()
+}
